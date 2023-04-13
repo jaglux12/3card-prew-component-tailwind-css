765861e33dfa75392f56b68982337f5620e384fd
jaglux12 <gonzalopez239@gmail.com>
2023-04-13 14:53:42 -0300

    Add main files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      padding: {
+        '3.2rem':'3.2rem',
+      },
+      spacing: {
+        '327':'20.4rem',
+        '442':'27.6rem',
+        '146':'9.1rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
